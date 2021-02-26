b62a33a762181fe221de8da2470518173f28bf9c
BG_KANG <79246255+lovelybg0506@users.noreply.github.com>
2021-02-26 17:05:20 +0900

    Add files via upload

---
+++ Chapter03/src/part01/sec01/exam02/Ex03_11.java
@@ -0,0 +1,28 @@
+package part01.sec01.exam02;
+
+public class Ex03_11 {
+
+	public static void main(String[] args) {
+
+		/*
+		int a,b,c,d,e;
+		
+		a='A'; // 65
+		System.out.printf("%c\t",a); //char
+		System.out.printf("%d\t",a); //정수출력
+		
+//		char a,b,c,d,e; // 타입이 달라도 위와 변수명이 동일해서 안됨.
+		
+		*/
+		
+		char a,b,c,d,e;
+		a='A'; // 65
+		System.out.printf("%c\t", a);
+		System.out.printf("%d\t", (int)a); // a를 캐스팅하지않으면 에러가남.
+		
+		b='a'; //97
+//		c=b+1;
+
+	}
+
+}
